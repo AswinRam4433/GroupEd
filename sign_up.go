39086fe2e331fc862eab4ffef443440da3fd3214
AswinRam4433 <aswinarv55@gmail.com>
2023-10-13 22:09:33 +0530

    Form Validation

---
+++ sign_up.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	// "./new_back.go"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+)
+
+// Full package path
+
+type FormData struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Discord   string `json:"discord"`
+	Phno      string `json:"phno"`
+	Subscribe string `json:"subscribe"`
+	Country   string `json:"country"`
+	Gender    string `json:"gender"`
+}
+
+func submitForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var formData FormData
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	formData.Name = r.FormValue("name")
+	formData.Email = r.FormValue("email")
+	formData.Discord = r.FormValue("discord")
+	formData.Phno = r.FormValue("phno")
+	formData.Subscribe = r.FormValue("subscribe")
+	formData.Country = r.FormValue("country")
+	formData.Gender = r.FormValue("gender")
+
+	// Process the form data as needed
+	fmt.Printf("Received data: %+v\n", formData)
+	fmt.Println("The discord ID is ", formData.Discord)
+	enc_disc, err := GetAESEncrypted(formData.Discord)
+	fmt.Println(enc_disc)
+	// w.WriteHeader(http.StatusOK)
+	// io.WriteString(w, "Form data received")
+	answer := valFormData(formData)
+	print("Result of answer is ", answer)
+
+	http.Redirect(w, r, "/otp.html", http.StatusSeeOther)
+
+	// sendOtp(string(formData.Phno))
+
+}
+
+func valFormData(f FormData) bool {
+	if f.Name != "" && f.Discord != "" {
+		return true
+	}
+	return false
+}
+
+func GetAESDecrypted(encrypted string) ([]byte, error) {
+	key := "my32digitkey12345678901234567890"
+	iv := "my16digitIvKey12"
+
+	ciphertext, err := base64.StdEncoding.DecodeString(encrypted)
+
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext)%aes.BlockSize != 0 {
+		return nil, fmt.Errorf("block size can't be zero")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, []byte(iv))
+	mode.CryptBlocks(ciphertext, ciphertext)
+	ciphertext = PKCS5UnPadding(ciphertext)
+
+	return ciphertext, nil
+}
+
+// PKCS5UnPadding  pads a certain blob of data with necessary data to be used in AES block cipher
+func PKCS5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+
+	return src[:(length - unpadding)]
+}
+
+// GetAESEncrypted encrypts given text in AES 256 CBC
+func GetAESEncrypted(plaintext string) (string, error) {
+	key := "my32digitkey12345678901234567890"
+	iv := "my16digitIvKey12"
+
+	var plainTextBlock []byte
+	length := len(plaintext)
+
+	if length%16 != 0 {
+		extendBlock := 16 - (length % 16)
+		plainTextBlock = make([]byte, length+extendBlock)
+		copy(plainTextBlock[length:], bytes.Repeat([]byte{uint8(extendBlock)}, extendBlock))
+	} else {
+		plainTextBlock = make([]byte, length)
+	}
+
+	copy(plainTextBlock, plaintext)
+	block, err := aes.NewCipher([]byte(key))
+
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, len(plainTextBlock))
+	mode := cipher.NewCBCEncrypter(block, []byte(iv))
+	mode.CryptBlocks(ciphertext, plainTextBlock)
+
+	str := base64.StdEncoding.EncodeToString(ciphertext)
+
+	return str, nil
+}
+
+// func main() {
+// 	r := http.NewServeMux()
+// 	// http.Handle("/", http.FileServer(http.Dir("static")))
+// 	r.HandleFunc("/submit-form", submitForm)
+
+// 	http.Handle("/", r)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+func main() {
+	// Serve the HTML file
+	http.Handle("/", http.FileServer(http.Dir("static")))
+
+	// Handle form submissions
+	http.HandleFunc("/submit-form", submitForm)
+
+	fmt.Println("Server is running on :8080...")
+	http.ListenAndServe(":8080", nil)
+}
