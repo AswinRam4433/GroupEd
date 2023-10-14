2403ff431d68f2c6fc98b913252892f6c83b66ca
AswinRam4433 <aswinarv55@gmail.com>
2023-10-14 15:32:08 +0530

    OTP Error

--- sign_up.go
+++ sign_up.go
@@ -9,10 +9,79 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+
+	"github.com/joho/godotenv"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/verify/v2"
 )
 
 // Full package path
+
+var TWILIO_ACCOUNT_SID string
+var TWILIO_AUTH_TOKEN string
+var VERIFY_SERVICE_SID string
+
+var client *twilio.RestClient
+
+func init() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
+
+	// Initialize environment variables
+	TWILIO_ACCOUNT_SID = os.Getenv("TWILIO_ACCOUNT_SID")
+	TWILIO_AUTH_TOKEN = os.Getenv("TWILIO_AUTH_TOKEN")
+	VERIFY_SERVICE_SID = os.Getenv("VERIFY_SERVICE_SID")
+
+	// Initialize Twilio client
+	client = twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: TWILIO_ACCOUNT_SID,
+		Password: TWILIO_AUTH_TOKEN,
+	})
+
+	// fmt.Println((client))
+}
+
+func sendOtp(to string) {
+	fmt.Println("Sending OTP")
+
+	params := &openapi.CreateVerificationParams{}
+	params.SetTo(to)
+	params.SetChannel("sms")
+
+	resp, err := client.VerifyV2.CreateVerification(VERIFY_SERVICE_SID, params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("Sent verification '%s'\n", *resp.Sid)
+	}
+}
+
+func checkOtp(to string) {
+	var code string
+	fmt.Println("Please check your phone and enter the code:")
+	fmt.Scanln(&code)
+
+	params := &openapi.CreateVerificationCheckParams{}
+	params.SetTo(to)
+	params.SetCode(code)
+
+	resp, err := client.VerifyV2.CreateVerificationCheck(VERIFY_SERVICE_SID, params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if *resp.Status == "approved" {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect!")
+	}
+}
 
 type FormData struct {
 	Name      string `json:"name"`
@@ -80,10 +149,13 @@ func otpVerificationPage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalf("Error occured while reading cookie")
 	}
-	fmt.Println("\nPrinting cookie with name as token")
+	fmt.Println("\nPrinting cookie with phone number as token")
 	fmt.Println(tokenCookie)
+	fmt.Println("The key is ", tokenCookie.Name)
+	fmt.Println("The value is ", tokenCookie.Value)
 
-	// fmt.Println("\nPrinting all cookies")
+	sendOtp(tokenCookie.Value)
+	fmt.Println("\nSending the OTP")
 	// for _, c := range r.Cookies() {
 	// 	fmt.Println(c)
 	// }
